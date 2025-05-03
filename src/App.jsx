3cbae8010570cb2c2d62fb49f91766a516bf7400
Sunanda01 <sunandasadhukhan1@gmail.com>
2025-05-03 11:01:33 +0530

    feat: Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+function App() {
+  return <>
+  <Navbar/>
+  {/* <Login/> */}
+  <Register/>
+  </>;
+}
+
+export default App;
