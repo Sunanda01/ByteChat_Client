847dd11db2b7124d7e83e5b65da6f609878a8335
Sunanda01 <sunandasadhukhan1@gmail.com>
2025-05-06 14:42:38 +0530

    small test

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,38 @@ import { defineConfig } from "vite";
 import path from "path"
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from 'vite-plugin-pwa'
 // https://vite.dev/config/
+
+const manifestForPlugin={
+  registerTypes:"prompt",
+  manifest:{
+    "short_name": "ByteChat",
+    "name": "ByteChat",
+    "description":"Chat App",
+    "icons": [
+      {
+        src: "/ByteChat.png",
+        sizes: "192x192",
+        type: "image/png",
+     },
+     {
+        src: "/ByteChat.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "favicon"
+     },
+  ],
+  theme_color: "#2196f3",
+  background_color: "#2196f3",
+  display: "standalone",
+  scope: "/",
+  start_url: "/"
+}
+}
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),VitePWA(manifestForPlugin)],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
